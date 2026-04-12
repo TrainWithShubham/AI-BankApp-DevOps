c4b50541f615e927b0d3eba27ba9a4104d9ba324
srdangat <sanket.r.dangat@gmail.com>
2026-04-12 23:49:55 +0530

    Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_version" {
 variable "node_instance_type" {
   description = "EC2 instance type for EKS worker nodes"
   type        = string
-  default     = "t3.medium"
+  default     = "m7i-flex.large"
 }
 
 variable "node_desired_count" {
