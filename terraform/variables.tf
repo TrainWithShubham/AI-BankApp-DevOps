87ba4e9edf50129ae538c5ab54b8643a7f613fb9
PRAVEEN SINGH TOMAR <tomarpraveen640@gmail.com>
2026-06-09 13:24:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,11 +25,17 @@ variable "node_instance_type" {
 variable "node_desired_count" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_max_count" {
   description = "Maximum number of worker nodes"
   type        = number
   default     = 3
+}
+
+variable "node_min_count" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 2
 }
